3202818c757138660e39e9c5e638ce62f9a6fb23
Kevin Kabatra <kevin@lsretail.com>
2019-11-10 19:21:40 +0000

    Updated project settings for WebGL build
    1. Added Editor Script so that I could included embedded resources in my WebGL build.

---
+++ Assets/Editor/WebGLEditorScript.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Editor
+{
+    using UnityEditor;
+
+    public class WebGLEditorScript
+    {
+        [MenuItem("WebGL/Enable Embedded Resources")]
+        public static void EnableErrorMessageTesting()
+        {
+            PlayerSettings.SetPropertyBool("useEmbeddedResources", true, BuildTargetGroup.WebGL);
+        }
+    }
+}
